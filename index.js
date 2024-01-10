042b479b486e4f20e02f0797ff05c6898b6169d6
Archlizz <liaozj1@yusys.com.cn>
2024-01-10 10:49:42 +0800

    fix(): 添加主文件

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: Archlizz liaozj1@yusys.com.cn
+ * @Date: 2024-01-10 10:49:11
+ * @LastEditors: Archlizz liaozj1@yusys.com.cn
+ * @LastEditTime: 2024-01-10 10:49:21
+ * @FilePath: /release-it-test/index.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+console.log("this is a test!!!");
